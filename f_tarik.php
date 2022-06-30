e7cd40c0a91d65db66186c4b03e87e7a3a39f277
gtpesantren22 <cakromat26@gmail.com>
2022-06-30 09:04:31 +0700

    Update tarik data

--- f_tarik.php
+++ f_tarik.php
@@ -7,7 +7,7 @@ if ($qr ==  'exc') {
     mysqli_query($conn, "DROP TABLE IF EXISTS datas ");
     $sql = mysqli_query($conn, "CREATE TABLE datas AS SELECT a.*, b.t_kos, c.kamar, c.komplek FROM psb22.tb_santri a JOIN psb22.dekos b ON a.nis=b.nis JOIN psb22.lemari_data c ON a.nis=c.nis WHERE a.ket = 'baru' ");
 
-    // $sql =  mysqli_query($conn, "CREATE VIEW datas AS SELECT a.*, b.t_kos, c.kamar, c.komplek FROM u9048253_psb22.tb_santri a JOIN u9048253_psb22.dekos b ON a.nis=b.nis JOIN u9048253_psb22.lemari_data c ON a.nis=c.nis WHERE a.ket = 'baru' ");
+    // $sql =  mysqli_query($conn, "CREATE TABLE datas AS SELECT a.*, b.t_kos, c.kamar, c.komplek FROM u9048253_psb22.tb_santri a JOIN u9048253_psb22.dekos b ON a.nis=b.nis JOIN u9048253_psb22.lemari_data c ON a.nis=c.nis WHERE a.ket = 'baru' ");
 
     if ($sql) {
         echo "
