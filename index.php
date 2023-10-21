10f822e171640eaba37727a50936a93b9317aab7
gtpesantren22 <cakromat26@gmail.com>
2023-10-21 20:15:40 +0700

    upate index page

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 include 'head.php';
+
+$snpa = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM tb_santri WHERE jkl = 'Laki-laki' AND aktif = 'Y' "));
+$snpi = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM tb_santri WHERE jkl = 'Perempuan' AND aktif = 'Y' "));
 ?>
 
 <div class="main-content">
@@ -59,86 +62,35 @@ include 'head.php';
 						<div class="col-sm-7 infobox-container">
 							<div class="infobox infobox-green">
 								<div class="infobox-icon">
-									<i class="ace-icon fa fa-comments"></i>
+									<i class="ace-icon fa fa-user"></i>
 								</div>
 
 								<div class="infobox-data">
-									<span class="infobox-data-number">32</span>
-									<div class="infobox-content">comments + 2 reviews</div>
+									<span class="infobox-data-number"><?= $snpa ?></span>
+									<div class="infobox-content">Santri Putra</div>
 								</div>
 
-								<div class="stat stat-success">8%</div>
 							</div>
 
 							<div class="infobox infobox-blue">
 								<div class="infobox-icon">
-									<i class="ace-icon fa fa-twitter"></i>
+									<i class="ace-icon fa fa-user"></i>
 								</div>
 
 								<div class="infobox-data">
-									<span class="infobox-data-number">11</span>
-									<div class="infobox-content">new followers</div>
+									<span class="infobox-data-number"><?= $snpi ?></span>
+									<div class="infobox-content">Santri Putri</div>
 								</div>
-
-								<div class="badge badge-success">
-									+32%
-									<i class="ace-icon fa fa-arrow-up"></i>
-								</div>
-							</div>
-
-							<div class="infobox infobox-pink">
-								<div class="infobox-icon">
-									<i class="ace-icon fa fa-shopping-cart"></i>
-								</div>
-
-								<div class="infobox-data">
-									<span class="infobox-data-number">8</span>
-									<div class="infobox-content">new orders</div>
-								</div>
-								<div class="stat stat-important">4%</div>
 							</div>
 
 							<div class="infobox infobox-red">
 								<div class="infobox-icon">
-									<i class="ace-icon fa fa-flask"></i>
+									<i class="ace-icon fa fa-users"></i>
 								</div>
 
 								<div class="infobox-data">
-									<span class="infobox-data-number">7</span>
-									<div class="infobox-content">experiments</div>
-								</div>
-							</div>
-
-							<div class="infobox infobox-orange2">
-								<div class="infobox-chart">
-									<span class="sparkline" data-values="196,128,202,177,154,94,100,170,224"></span>
-								</div>
-
-								<div class="infobox-data">
-									<span class="infobox-data-number">6,251</span>
-									<div class="infobox-content">pageviews</div>
-								</div>
-
-								<div class="badge badge-success">
-									7.2%
-									<i class="ace-icon fa fa-arrow-up"></i>
-								</div>
-							</div>
-
-							<div class="infobox infobox-blue2">
-								<div class="infobox-progress">
-									<div class="easy-pie-chart percentage" data-percent="42" data-size="46">
-										<span class="percent">42</span>%
-									</div>
-								</div>
-
-								<div class="infobox-data">
-									<span class="infobox-text">traffic used</span>
-
-									<div class="infobox-content">
-										<span class="bigger-110">~</span>
-										58GB remaining
-									</div>
+									<span class="infobox-data-number"><?= $snpa + $snpi ?></span>
+									<div class="infobox-content">Total Santri Aktif</div>
 								</div>
 							</div>
 
