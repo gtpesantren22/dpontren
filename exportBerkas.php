03943de1c41bcdf45410fc20953859bc88dcf8f3
gtpesantren22 <cakromat26@gmail.com>
2025-05-02 06:59:58 +0700

    update: update fungsi export berkas psb santri 24

--- exportBerkas.php
+++ exportBerkas.php
@@ -9,26 +9,23 @@ set_time_limit(0); // Menghilangkan batas waktu eksekusi
 $dataS = mysqli_fetch_assoc(mysqli_query($conn_psb, "SELECT * FROM tb_santri WHERE nis = $nis "));
 
 /// create a zip file
-$zip_file = "images/berkas_" . $dataS['nama'] . ".zip";
-touch($zip_file);
+$zipFilename = "images/berkas_" . $dataS['nama'] . ".zip";
+// touch($zip_file);
 // end
 
 
 // open zip file
 $zip = new ZipArchive;
-$this_zip = $zip->open($zip_file);
+// $this_zip = $zip->open($zip_file);
 
-
-if ($this_zip) {
-
-    // Query untuk mengambil data file dari database
-    $sql = "SELECT a.*, b.diri, b.ayah, b.ibu 
+$sql = "SELECT a.*, b.diri, b.ayah, b.ibu 
         FROM berkas_file a 
         LEFT JOIN foto_file b ON a.nis = b.nis 
         WHERE a.nis = '$nis'";
 
-    $result = $conn_psb->query($sql);
+$result = $conn_psb->query($sql);
 
+if ($zip->open($zipFilename, ZipArchive::CREATE) === TRUE) {
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             // Daftar file yang akan dicek dari tabel
@@ -46,25 +43,14 @@ if ($this_zip) {
             }
         }
     }
-
-    // Menutup ZIP
     $zip->close();
 
-
-    // download this created zip file
-    if (file_exists($zip_file)) {
-        ob_clean(); // Bersihkan buffer output
-        flush();
-        //name when download
-        $demo_name = $zip_file;
-
-        header('Content-type: application/zip');
-        header('Content-Disposition: attachment; filename="' . $demo_name . '"');
-        readfile($zip_file); // auto download
-
-        //delete this zip file after download
-        unlink($zip_file);
-    }
-
-    $conn->close();
+    // Download ZIP
+    header('Content-Type: application/zip');
+    header('Content-disposition: attachment; filename=' . $zipFilename);
+    header('Content-Length: ' . filesize($zipFilename));
+    readfile($zipFilename);
+    unlink($zipFilename); // hapus file zip setelah di-download
+} else {
+    echo "Gagal membuat file ZIP.";
 }
